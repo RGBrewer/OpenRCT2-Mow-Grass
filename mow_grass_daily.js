268410528059c24ebedd11aba5f8e7e06f9fc862
RGBrewer <RGBrewer@gmail.com>
2020-05-06 20:52:05 -0400

    Add files via upload

---
+++ mow_grass_daily.js
@@ -0,0 +1,24 @@
+var mowGrass = function() {
+	for (var y = 0; y < map.size.y; y++) {
+	    for (var x = 0; x < map.size.x; x++) {
+	        var tile = map.getTile(x, y);
+	        for (var i = 0; i < tile.numElements; i++) {
+	            var element = tile.getElement(i);
+	            element.grassLength = 8;
+	        }
+	    }
+	}
+}
+var main = function() {
+    var tickCount = 0;
+    var daySubscription = context.subscribe('interval.day', function() {
+		mowGrass();
+    });
+};
+registerPlugin({
+    name: 'Mow Grass Every Day',
+    version: '1.0',
+    authors: ['OpenRCT2'],
+    type: 'remote',
+    main: main
+});
